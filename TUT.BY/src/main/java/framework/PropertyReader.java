42fae5ca435ad4e5a26bb8495e2d1787fdca8a18
Yaroslav Lazakovich <Buslikx10@gmail.com>
2019-04-02 12:47:33 +0300

    update config.properties & PropertyReader.java

---
+++ TUT.BY/src/main/java/framework/PropertyReader.java
@@ -0,0 +1,64 @@
+package framework;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertyReader {
+    private final InputStream path = this.getClass().getClassLoader().getResourceAsStream("config.properties");
+
+    private static final Object lock = new Object();
+    private static PropertyReader instance;
+
+    private String url;
+    private String browserName;
+    private long shortTimeOut;
+    private long longTimeOut;
+    private String host;
+    private String sqlLogin;
+    private String sqlPassword;
+
+
+    public static PropertyReader getInstance() {
+        if (instance == null) {
+            synchronized (lock) {
+                instance = new PropertyReader();
+                instance.loadData();
+            }
+        }
+        return instance;
+    }
+
+    private void loadData() {
+        Properties properties = new Properties();
+        try {
+            properties.load(path);
+        } catch (IOException e) {
+            System.err.println("Configuration properties file cannot be found");
+        }
+
+        url = properties.getProperty("url");
+        browserName = properties.getProperty("browserName");
+        shortTimeOut = Long.parseLong(properties.getProperty("shortTimeOut"));
+        longTimeOut = Long.parseLong(properties.getProperty("longTimeOut"));
+        host = properties.getProperty("db.host");
+        sqlLogin = properties.getProperty("db.login");
+        sqlPassword = properties.getProperty("db.password");
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getBrowserName() {
+        return browserName;
+    }
+
+    public long getShortTimeOut() {
+        return shortTimeOut;
+    }
+
+    public long getLongTimeOut() {
+        return longTimeOut;
+    }
+}
