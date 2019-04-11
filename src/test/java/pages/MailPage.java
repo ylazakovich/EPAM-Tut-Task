7ac21c04de9dab89ca1c7a6fc121b4831ae9dc50
Yaroslav Lazakovich <Buslikx10@gmail.com>
2019-04-11 23:42:14 +0300

    update PageObject

--- src/test/java/pages/MailPage.java
+++ src/test/java/pages/MailPage.java
@@ -1,10 +1,13 @@
 package pages;
 
+import framework.PropertyReader;
 import framework.browserFactory.utils.Waiter;
+import framework.browserFactory.utils.WebDriverManager;
 import framework.elements.Label;
+import framework.elements.Title;
 import framework.pageObject.BasePage;
 import org.openqa.selenium.By;
-import pages.folders.SentFolder;
+import org.testng.Assert;
 
 public class MailPage extends BasePage {
     private static By mailPageLocator = By.xpath("//div[@class='mail-User-Name']");
@@ -12,6 +15,17 @@ public class MailPage extends BasePage {
     private By nestedLocator = By.xpath("//div[@data-key='view=folders']");
     private By nestedItem = By.xpath("//span[@class='mail-NestedList-Item-Name']");
     private Label nestedList;
+
+    // Inbox Folder
+    private By messageInboxFolder = By.xpath("//div[contains(@class,'mail-MessagesList')]");
+    private By messageLine = By.xpath("//div[@class='mail-MessageSnippet-Content']");
+    private Label inboxFolderLabel;
+
+    // Sent Folder
+    private By recipientEmails = By.xpath("//span[@class='mail-MessageSnippet-FromText']");
+    private By subjectColumn = By.xpath("//span[contains(@class,'mail') and contains(@class, 'Item_subject')]/span[@title]");
+    private Title recipientTitles = new Title(recipientEmails);
+    private Title subjectTitles = new Title(subjectColumn);
 
     public MailPage() {
         super(mailPageLocator);
@@ -21,11 +35,33 @@ public class MailPage extends BasePage {
         return mailPageLocator;
     }
 
-    public SentFolder goToSentFolder() {
+    public LoginPage goToLoginPage() {
+        WebDriverManager.openUrl(getDriver(), PropertyReader.getProperty("url"));
+        Waiter.fluentWait(getDriver(), LoginPage.getLoginPageLocator());
+        return new LoginPage();
+    }
+
+    public MailPage goToSentFolder() {
         nestedList = new Label(nestedLocator);
         nestedList.getElements(nestedItem).get(1).click();
-        Waiter.fluentWait(getDriver(), SentFolder.getSentFolderLocator());
-        return new SentFolder();
+        Waiter.fluentWait(getDriver(), getMailPageLocator());
+        return this;
+    }
+
+    public MessagePage goToFirstMessage() {
+        Label inboxFolderLabel = new Label(messageInboxFolder);
+        inboxFolderLabel.getElements(messageLine).get(0).click();
+        Waiter.fluentWait(getDriver(), MessagePage.getMessagePageLocator());
+        return new MessagePage();
+    }
+
+    public MailPage assertSentMessage(String recipientEmail, String expectedSubject) {
+        String actualEmail = recipientTitles.getTextElements(recipientEmails).get(0);
+        Assert.assertEquals(actualEmail, recipientEmail);
+
+        String actualSubject = subjectTitles.getTextElements(subjectColumn).get(0);
+        Assert.assertEquals(actualSubject, expectedSubject);
+        return this;
     }
 
 }
