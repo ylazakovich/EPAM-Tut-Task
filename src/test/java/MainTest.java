84c875eb7199b0ceec842d814209f853e9ca8758
Yaroslav Lazakovich <Buslikx10@gmail.com>
2019-05-08 18:49:57 +0300

    add screenshot to readme.md

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -3,6 +3,7 @@ import framework.BaseTest;
 import framework.DataFactory;
 import framework.dataFactory.User;
 import framework.mail.JavaMail;
+import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Link;
 import org.testng.annotations.Listeners;
@@ -18,6 +19,9 @@ public class MainTest extends BaseTest {
     @Override
     @Link("https://www.tut.by/")
     @Link(name = "allure", type = "mylink")
+    @Description("Send e-mail from acc1 to acc2 (use Java Mail API) with text, sent it to acc2 via to/cc/bcc (all 3 options)\n" +
+            "Log in to acc1 from UI. Check e-mail in Sent present\n" +
+            "Log in to acc2 from UI. Check e-mail in Inbox and check text has valid message\n")
     public void run() {
         List<User> users = DataFactory.getUserList();
 
